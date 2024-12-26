14740a42a9ef5f33eeeef62a36cb229cd165bd9d
Lucayan20 <lucayanco@gmail.com>
2024-12-26 20:47:04 -0300

    Ajustes para deploy

--- server.js
+++ server.js
@@ -42,7 +42,6 @@ app.use((req, res, next) => {
 // Configuração do banco de dados
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
