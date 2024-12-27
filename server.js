20a1471e524c70c86134d1938cd2fb43f2a86311
Lucayan20 <lucayanco@gmail.com>
2024-12-27 20:43:22 -0300

    Descrição das alterações

--- server.js
+++ server.js
@@ -45,8 +45,10 @@ const db = mysql.createConnection({
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  connectTimeout: 30000  // 30 segundos
 });
+
 
 db.connect((err) => {
   if (err) {
